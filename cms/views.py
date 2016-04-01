923997e5b8ea66200a5ed27ff8d5c94cb1982126
Ana Alberca Diaz Plaza <AnaAlberca@MacBook-Pro-de-Ana.local>
2016-04-01 15:59:55 +0200

    Entrega con error

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def pagina(request, identificador):
+
+    try:
+        pag = Pages.objets.get(id = int(identificador))
+        respuesta = pag.page
+    except Pages.DoesNotExist:
+        respuesta = "No existe ese nombre con contenidos en la base de datos"
+    return HttpResponse(respuesta)
+
+def muestra_todo(request):
+    lista = Pages.objets.all()
+    respuesta = "<ol>"
+    for pag in lista:
+        respuesta += '<li><a href="' + str(pag.id) + '">' + pag.name + '</a>'
+    respuesta += "</ol>"
+    return HttpResponse(respuesta)
